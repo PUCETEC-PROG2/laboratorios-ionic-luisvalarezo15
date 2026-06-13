7b6418b120eb64fd104d8c4c4eba79d939221052
luisvalarezo15 <luisvalarezo2007@gmail.com>
2026-06-13 12:13:51 -0500

    Implementar lista de repositorios con RepoItem clase13/6/2026

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import RepoItem from "../components/RepoItem";
+import { repositoryList } from "../interfaces/Repository";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <IonList>
+          {repositoryList.map((repo) => (
+            <RepoItem key={repo.name} {...repo} />
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
