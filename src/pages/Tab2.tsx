f65c647b13e8c316cd1d6aa2760320dc946d4497
Usuario invitado <Guest@iMac-de-PUCE-A10814.local>
2026-06-16 14:58:19 -0500

    clase 16/6/26

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -7,15 +7,41 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Formulario de repositorio</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        <div className="form-field">
+          <IonInput
+            className="form-field"
+            label="Descripcion"
+            placeholder="Ingrese la descripccion del repositorio"
+            labelPlacement="floating"
+          />
+
+          <IonTextarea
+            className="form-field"
+            label="Descripcion"
+            placeholder="Ingrese la descripccion del repositorio"
+            labelPlacement="floating"
+            rows={4}
+          />
+
+          <IonButton
+            className="form-field"
+            expand="block"
+            fill="solid"
+          >
+            Enviar
+          </IonButton>
+        </div>
+
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardHeader } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -7,16 +7,31 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="card-container">
+          <IonCard className="card">
+            <img
+              src="https://avatars.githubusercontent.com/u/191405317?v=4&size=64"
+              alt="Foto de perfil"
+            />
+            <IonCardHeader className="card">
+              <IonCardTitle>Luis Valarezo</IonCardTitle>
+              <IonCardTitle>luisvalarezo15</IonCardTitle>
+            </IonCardHeader>
+            <p>
+              Estudiante de desarrollo de software en cuarto semestre en la
+              Pontificia Universidad Catolica del Ecuador
+            </p>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
